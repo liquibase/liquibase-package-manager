dc2ccc2175631e82cfa6d099ca753c5dc14afee9
Derek Smart <derek@grindaga.com>
2021-07-17 01:10:09 -0400

    init windows
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
-.PHONY: build
+.PHONY: build darwin windows
+
+windows:
+	GOOS=windows GOARCH=amd64 go build -o $(PWD)/bin/windows/lpm.exe $(PWD)/cmd/lpm/windows.go
+	zip ./bin/windows/lpm-$(VERSION)-windows.zip ./bin/windows/lpm.exe
+
+darwin:
+	GOOS=darwin GOARCH=amd64 go build -o $(PWD)/bin/darwin/lpm $(PWD)/cmd/lpm/darwin.go
+	zip ./bin/darwin/lpm-$(VERSION)-darwin.zip ./bin/lpm
 
 build:
 	go build -o $(PWD)/bin/lpm $(PWD)/cmd/lpm/darwin.go
---
+++ cmd/lpm/windows.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"package-manager/internal/app"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	var liquibasepath string
+
+	// Find Liquibase Command
+	out, err := exec.Command("where", "liquibase").CombinedOutput()
+	if err != nil {
+		fmt.Println("Unable to locate Liquibase.")
+		os.Exit(1)
+	}
+
+	// Determine if Command is Symlink
+	loc := strings.TrimRight(string(out), "\n")
+	fi, err := os.Lstat(loc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if fi.Mode()&os.ModeSymlink != 0 {
+		link, err := os.Readlink(loc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Is Symlink
+		liquibasepath, _ = filepath.Split(link)
+	} else {
+		// Not Symlink
+		liquibasepath, _ = filepath.Split(loc)
+	}
+
+	app.Exec(liquibasepath + "lib/")
+
+}
