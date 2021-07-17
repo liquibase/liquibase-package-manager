3d9b27a763421df40bfef2fa8a0eb93ffe122f95
Derek Smart <derek@grindaga.com>
2021-07-16 20:38:36 -0400

    can download or install from local
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/commands/install.go
+++ internal/app/commands/install.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"io"
 	"os"
 )
 
@@ -15,31 +14,23 @@ var installCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		name := args[0]
 		p := packs.GetByName(name)
+
 		if p.Name == "" {
 			fmt.Println("Package '" + name + "' not found.")
 			os.Exit(1)
 		}
+
 		if p.InClassPath(classpathFiles) {
 			fmt.Println(name + " is already installed.")
 			os.Exit(1)
 		}
-		source, err := os.Open(p.Path)
-		if err != nil {
-			fmt.Println("Unable to open " + p.Path)
-			os.Exit(1)
+
+		if !p.PathIsHttp() {
+			p.CopyToClassPath(classpath)
+		} else {
+			p.DownloadToClassPath(classpath)
 		}
-		defer source.Close()
-		destination, err := os.Create(classpath + p.GetFilename())
-		if err != nil {
-			fmt.Println("Unable to access classpath located at " + classpath)
-			os.Exit(1)
-		}
-		defer destination.Close()
-		_, err = io.Copy(destination, source)
-		if err != nil {
-			fmt.Println("Unable to install " + p.GetFilename() + " in classpath.")
-			os.Exit(1)
-		}
+
 		fmt.Println(p.GetFilename() + " successfully installed in classpath.")
 	},
 }
--- internal/app/packages/Package.go
+++ internal/app/packages/Package.go
@@ -1,8 +1,15 @@
 package packages
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"io"
 	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Package struct {
@@ -25,4 +32,57 @@ func (p Package) InClassPath(cp []fs.FileInfo) bool {
 		}
 	}
 	return r
+}
+
+func (p Package) PathIsHttp() bool {
+	return strings.HasPrefix(p.Path, "http")
+}
+
+func writeToDestination(d string, r io.Reader, f string) {
+	destination, err := os.Create(d)
+	if err != nil {
+		fmt.Println("Unable to access classpath located at " + d)
+		os.Exit(1)
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, r)
+	if err != nil {
+		fmt.Println("Unable to install " + f + " in classpath.")
+		os.Exit(1)
+	}
+}
+
+func (p Package) CopyToClassPath(cp string) {
+	source, err := os.Open(p.Path)
+	if err != nil {
+		fmt.Println("Unable to open " + p.Path)
+		os.Exit(1)
+	}
+	defer source.Close()
+	writeToDestination(cp + p.GetFilename(), source, p.GetFilename())
+}
+
+func (p Package) DownloadToClassPath(cp string) {
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+	r, err := client.Get(p.Path)
+	if err != nil {
+		fmt.Println("Unable to download from " + p.Path)
+		os.Exit(1)
+
+	}
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	sha := fmt.Sprintf("%x", sha1.Sum(body))
+	if sha == p.CheckSum {
+		fmt.Println("Checksum verified. Installing " + p.GetFilename() + " to " + cp)
+	} else {
+		fmt.Println("Checksum validation failed. Aborting download.")
+		os.Exit(1)
+	}
+	writeToDestination(cp + p.GetFilename(), r.Body, p.GetFilename())
 }
