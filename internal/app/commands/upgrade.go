505e63544c18b42ea56389ee0412838ad60c4d09
Derek Smart <derek@grindaga.com>
2022-11-21 16:36:09 -0500

    can output outdated packages
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/commands/upgrade.go
+++ internal/app/commands/upgrade.go
@@ -3,20 +3,51 @@ package commands
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"package-manager/internal/app"
+	"strconv"
+)
+
+var (
+	dryRun bool
 )
 
 // upgradeCmd represents the update command
 var upgradeCmd = &cobra.Command{
-	Use:   "upgrade [PACKAGE]...",
-	Short: "Upgrades Installed Packages to the Latest Versions",
+	Use:     "upgrade [PACKAGE]...",
+	Short:   "Upgrades Installed Packages to the Latest Versions",
+	Aliases: []string{"up"},
 
 	Run: func(cmd *cobra.Command, args []string) {
-		//todo implement this
-		fmt.Println("upgrade")
+		outdated := packs.GetOutdated(liquibase.Version, app.ClasspathFiles)
+		if len(outdated) == 0 {
+			fmt.Println("You have no outdated packages installed.")
+			fmt.Println(app.Classpath)
+			os.Exit(0)
+		}
+		var r []string
+		var prefix string
+		r = append(r, fmt.Sprintf("%-4s %-38s %-38s %s", "   ", "Package", "Installed", "Available"))
+		for i, p := range outdated {
+			ins := p.GetInstalledVersion(app.ClasspathFiles)
+			latest := p.GetLatestVersion(liquibase.Version)
+			if (i + 1) == len(outdated) {
+				prefix = "└──"
+			} else {
+				prefix = "├──"
+			}
+			r = append(r, fmt.Sprintf("%-4s %-38s %-38s %s", prefix, p.Name, ins.Tag, latest.Tag))
+		}
+		fmt.Println("You have " + strconv.Itoa(len(outdated)) + " outdated package(s) installed.")
+		fmt.Println(app.Classpath)
+		for _, out := range r {
+			fmt.Println(out)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(upgradeCmd)
 	upgradeCmd.Flags().BoolVarP(&global, "global", "g", false, "upgrade global packages")
+	upgradeCmd.Flags().BoolVar(&dryRun, "dry-run", false, "output changes without applying")
 }
--- internal/app/packages/Packages.go
+++ internal/app/packages/Packages.go
@@ -2,13 +2,14 @@ package packages
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-version"
 	"io/fs"
 )
 
-//Packages type
+// Packages type
 type Packages []Package
 
-//GetByName individual package from packages
+// GetByName individual package from packages
 func (ps Packages) GetByName(n string) Package {
 	var r Package
 	for _, p := range ps {
@@ -19,7 +20,34 @@ func (ps Packages) GetByName(n string) Package {
 	return r
 }
 
-//FilterByCategory get packages by catetory
+// GetInstalled get slice of installed packages in classpath
+func (ps Packages) GetInstalled(cpFiles []fs.FileInfo) Packages {
+	var r Packages
+	for _, e := range ps {
+		v := e.GetInstalledVersion(cpFiles)
+		if v.InClassPath(cpFiles) {
+			r = append(r, e)
+		}
+	}
+	return r
+}
+
+// GetOutdated get slice of outdated packaged in classpath
+func (ps Packages) GetOutdated(lb *version.Version, cpFiles []fs.FileInfo) Packages {
+	var r Packages
+	for _, p := range ps.GetInstalled(cpFiles) {
+		i := p.GetInstalledVersion(cpFiles)
+		iv, _ := version.NewVersion(i.Tag)
+		l := p.GetLatestVersion(lb)
+		lv, _ := version.NewVersion(l.Tag)
+		if iv.LessThan(lv) {
+			r = append(r, p)
+		}
+	}
+	return r
+}
+
+// FilterByCategory get packages by catetory
 func (ps Packages) FilterByCategory(c string) Packages {
 	var r Packages
 	for _, p := range ps {
@@ -30,13 +58,13 @@ func (ps Packages) FilterByCategory(c string) Packages {
 	return r
 }
 
-//Display generate display table for packages
+// Display generate display table for packages
 func (ps Packages) Display(files []fs.FileInfo) []string {
 	var r []string
 	var prefix string
 	r = append(r, fmt.Sprintf("%-4s %-38s %s", "   ", "Package", "Category"))
 	for i, p := range ps {
-		if (i+1) == len(ps) {
+		if (i + 1) == len(ps) {
 			prefix = "└──"
 		} else {
 			prefix = "├──"
@@ -48,7 +76,7 @@ func (ps Packages) Display(files []fs.FileInfo) []string {
 		} else {
 			v = tag
 		}
-		r = append(r, fmt.Sprintf("%-4s %-38s %s", prefix, p.Name + v, p.Category))
+		r = append(r, fmt.Sprintf("%-4s %-38s %s", prefix, p.Name+v, p.Category))
 	}
 	return r
 }
