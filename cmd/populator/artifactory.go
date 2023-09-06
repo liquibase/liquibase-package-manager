0ba9eb2973007cfaeecc2b2fa487a6684477e3ba
Derek Smart <derek@grindaga.com>
2023-09-06 10:44:01 -0400

    update dependency gen

--- cmd/populator/artifactory.go
+++ cmd/populator/artifactory.go
@@ -11,13 +11,13 @@ import (
 	"strings"
 )
 
-//Artifactory main interface for module artifactory logic
+// Artifactory main interface for module artifactory logic
 type Artifactory interface {
 	GetVersions(Module) []*version.Version
 	GetNewVersions(Module, packages.Package) packages.Package
 }
 
-//GetPomFromURL get POM object from remote URL
+// GetPomFromURL get POM object from remote URL
 func GetPomFromURL(url string) gopom.Project {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -36,13 +36,13 @@ func GetPomFromURL(url string) gopom.Project {
 	return pom
 }
 
-//GetCoreVersionFromPom get liquibase core version string from POM object
+// GetCoreVersionFromPom get liquibase core version string from POM object
 func GetCoreVersionFromPom(pom gopom.Project) string {
 	var version string
-	for _, dep := range pom.Dependencies {
-		if dep.ArtifactID == "liquibase-core" {
-			if strings.Contains(dep.Version, "${") {
-				v := strings.TrimPrefix(dep.Version, "${")
+	for _, dep := range *pom.Dependencies {
+		if *dep.ArtifactID == "liquibase-core" {
+			if strings.Contains(*dep.Version, "${") {
+				v := strings.TrimPrefix(*dep.Version, "${")
 				v = strings.TrimSuffix(v, "}")
 				for k, prop := range pom.Properties.Entries {
 					if k == v {
@@ -50,7 +50,7 @@ func GetCoreVersionFromPom(pom gopom.Project) string {
 					}
 				}
 			} else {
-				version = dep.Version
+				version = *dep.Version
 			}
 		}
 	}
